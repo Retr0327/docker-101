8357ee1e9a76d05e58f58a61c22f98bfe79615a6
Retr0327 <retr0327.dev@gmail.com>
2023-10-27 04:47:25 +0800

    Add asset prefix

--- next.config.js
+++ next.config.js
@@ -2,12 +2,14 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
 });
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['tsx', 'mdx'],
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '/docker-101',
+  assetPrefix: isProduction ? 'https://retr0327.github.io/docker-101' : '/',
   output: 'export',
   images: { unoptimized: true },
 };
